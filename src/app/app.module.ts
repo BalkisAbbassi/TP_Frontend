65e0dd3181452800f0526aeebf84da80699ea2ac
BalkisAbbassi <blkabbassi@gmail.com>
2021-04-30 23:49:24 +0100

    tp partie2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AddComponent } from './add/add.component';
-import { ProductsComponent } from './products/products.component';
-import { UpdateProductComponent } from './update-product/update-product.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductsComponent } from './products/products.component';
+import { AppComponent } from './app.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddComponent,
     ProductsComponent,
     UpdateProductComponent
   ],
